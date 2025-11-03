6abe985d4281146c48704d6ec2ba02f1fd84786c
Vidur Shah <64993588+VidurShah@users.noreply.github.com>
2025-11-03 01:24:06 -0500

    Address Autocomplete & Geolocation (#51)
    * asdf
    
    * added location funcitons for autocomplete and location data
    
    * filepath dependency update
    
    Co-authored-by: Nicolas Asanov <98969136+naasanov@users.noreply.github.com>
    
    * implemented some reccomended changes
    
    * yaml update
    
    * created new exceptions
    
    * small change from address_component to address_components. Fields populating as None otherwise
    
    ---------
    
    Co-authored-by: Nicolas Asanov <98969136+naasanov@users.noreply.github.com>
    Co-authored-by: Nick A <nicolas.a.asanov@gmail.com>

--- backend/src/core/exceptions.py
+++ backend/src/core/exceptions.py
@@ -34,3 +34,8 @@ class CredentialsException(HTTPException):
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+class InternalServerException(HTTPException):
+    def __init__(self, detail: str):
+        super().__init__(status_code=500, detail=detail)
