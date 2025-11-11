2b5c3af417783258f3c91db2a28808b0d9a3b03c
MasonMines2006 <mmines@unc.edu>
2025-11-11 15:37:41 -0500

    Changed API keys and added basic sidebar functionality

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "@/components/GlobalSidebar";
+import { SidebarProvider } from "@/components/SidebarContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          {" "}
+          {children}
+          <Sidebar />
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ frontend/src/components/SidebarContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type SidebarContextType = {
+  isOpen: boolean;
+  content: ReactNode | null;
+  openSidebar: (content: ReactNode) => void;
+  closeSidebar: () => void;
+};
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export function SidebarProvider({ children }: { children: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState<ReactNode | null>(null);
+
+  const openSidebar = (content: ReactNode) => {
+    setContent(content);
+    setIsOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsOpen(false);
+    setContent(null);
+  };
+
+  return (
+    <SidebarContext.Provider
+      value={{ isOpen, content, openSidebar, closeSidebar }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+}
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context)
+    throw new Error("useSidebar must be used within SidebarProvider");
+  return context;
+};
