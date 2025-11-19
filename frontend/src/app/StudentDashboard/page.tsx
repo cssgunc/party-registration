a6e9c1dc07766c6b62e6ccf889fd805810c8cbe7
Aileen Rashid <aileenr@unc.edu>
2025-11-19 08:10:30 -0500

    styling

--- frontend/src/app/StudentDashboard/page.tsx
+++ frontend/src/app/StudentDashboard/page.tsx
@@ -1,24 +1,30 @@
+import OCSLLogo from "@/components/icons/OCSL_logo.svg";
+import pfp from "@/components/icons/pfp_temp.svg";
 import RegistrationTracker from "@/components/RegistrationTracker";
 import StatusComponent from "@/components/StatusComponent";
 import { PARTIES } from "@/lib/mockData";
+import Image from "next/image";
 import Link from "next/link";
 export default function StudentDashboard() {
     return (
-        <div className="px-20 py-2 flex flex-col gap-8 max-w-4xl mx-auto">
-            <div className="bg-stone-800 h-16 w-full"></div>
+        <div className="px-48 pb-12 flex flex-col gap-4 max-w-4xl mx-auto">
+            <div className="bg-[#6FB2DC] h-20 w-full flex justify-between items-center">
+                <Image src={OCSLLogo} alt="OCSL logo" className="pl-4 pt-7" />
+                <Image src={pfp} alt="pfp" className="pr-4 pt-6" width={60} height={60} />
+            </div>
 
             <div className="flex justify-between items-center">
                 <div className="font-semibold text-2xl">Events</div>
 
                 <Link href="/RegistrationForm">
-                    <button className="px-4 py-2 rounded-lg bg-black text-white">
+                    <button className="px-4 py-2 rounded-lg bg-[#09294E] text-white">
                         Registration Form
                     </button>
                 </Link>
             </div>
 
             <RegistrationTracker parties={PARTIES.filter(party => party.contactOne.id === 4)} />
-            <div>Party Smart Course </div>
+            <div className="text-[24px] font-semibold">Party Smart Course </div>
             <StatusComponent completion_date={null} expiration_date={null} />
         </div>
 
