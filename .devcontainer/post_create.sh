75cc9a48f1cf561a6c539165dbf6098d00ff25b4
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-01-06 08:44:04 -0500

    chore: add configuration and dev container setup for pre-commit hooks

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -e
 
-cd ../frontend
+# Expected to run from .devcontainer/
+
+echo "=============== Installing pre-commit hooks ==============="
+cd ..
+pre-commit install
+
+echo ""
+echo "=============== Installing frontend dependencies ==============="
+cd ./frontend
 npm i --verbose
 
+echo ""
+echo "=============== Setting up the database ==============="
 cd ../backend
 python -m script.create_db
 python -m script.create_test_db
--- backend/script/create_db.py
+++ backend/script/create_db.py
@@ -1,6 +1,6 @@
+from sqlalchemy import create_engine, text
 from src.core.config import env
 from src.core.database import server_url
-from sqlalchemy import create_engine, text
 
 engine = create_engine(server_url(sync=True), isolation_level="AUTOCOMMIT")
 
