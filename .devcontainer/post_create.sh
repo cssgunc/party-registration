9ce9ccf9f3a34191192a6021a76115a2ef432404
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-01-08 13:41:47 -0500

    refactor: fix pre commit hooks versioning and containerization

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,9 +3,10 @@ set -e
 
 # Expected to run from .devcontainer/
 
-echo "=============== Installing pre-commit hooks ==============="
+echo "================== Installing pre-commit hooks ================="
 cd ..
 pre-commit install
+pre-commit install-hooks
 
 echo ""
 echo "=============== Installing frontend dependencies ==============="
@@ -13,7 +14,7 @@ cd ./frontend
 npm i --verbose
 
 echo ""
-echo "=============== Setting up the database ==============="
+echo "==================== Setting up the database ==================="
 cd ../backend
 python -m script.create_db
 python -m script.create_test_db
