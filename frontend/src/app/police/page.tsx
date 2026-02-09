47f1877fc2e20f37f3553091ff92a260785d2ba4
Mereck Fandohan <fandohan@ad.unc.edu>
2026-02-08 23:11:34 -0500

    built base changes to create mobile layout and added new split date component for mobile view

--- frontend/src/app/police/page.tsx
+++ frontend/src/app/police/page.tsx
@@ -4,6 +4,7 @@ import EmbeddedMap from "@/app/police/_components/EmbeddedMap";
 import PartyList from "@/app/police/_components/PartyList";
 import AddressSearch from "@/components/AddressSearch";
 import DateRangeFilter from "@/components/DateRangeFilter";
+import SplitDateComponent from "@/app/police/_components/SplitDateComponent";
 import { LocationService } from "@/lib/api/location/location.service";
 import { AutocompleteResult } from "@/lib/api/location/location.types";
 import { PartyService } from "@/lib/api/party/party.service";
@@ -89,27 +90,38 @@ export default function PolicePage() {
   }, [activeParty]);
 
   return (
-    <div className="h-screen bg-white flex flex-col overflow-hidden">
+    <div className="md:h-screen bg-white flex flex-col overflow-hidden">
       {/* Navbar */}
       <div className="w-full bg-[#6FB2DC] h-16 flex-shrink-0"></div>
 
-      <div className="flex flex-1 overflow-hidden">
+      <div className="overflow-y-scroll md:flex flex-1 overflow-hidden">
         {/* Left Panel - Filters and Search */}
-        <div className="w-1/3 border-r border-gray-200 flex flex-col overflow-hidden">
+        <div className=" md:w-1/3 border-r border-gray-200 flex flex-col overflow-hidden">
           {/* Filter Between Section */}
-          <div className="px-6 py-4 flex-shrink-0 border-b border-gray-200">
-            <h2 className="text-xl font-semibold mb-4">Filter Between</h2>
-            <DateRangeFilter
-              startDate={startDate}
-              endDate={endDate}
-              onStartDateChange={setStartDate}
-              onEndDateChange={setEndDate}
-            />
+          <div className="px-4 md:px-6 py-4 flex-shrink-0 border-b border-gray-200">
+            <h2 className="text-2xl md:text-xl font-semibold mb-4">Filter Between</h2>
+            <div className="hidden md:flex">
+              <DateRangeFilter
+                startDate={startDate}
+                endDate={endDate}
+                onStartDateChange={setStartDate}
+                onEndDateChange={setEndDate}
+              />
+            </div>
+            <div className="mx-2">
+              <SplitDateComponent
+                startDate={startDate}
+                endDate={endDate}
+                onStartDateChange={setStartDate}
+                onEndDateChange={setEndDate}
+              />
+            </div>
+
           </div>
 
           {/* Dto Search Section */}
-          <div className="px-6 py-4 flex-shrink-0 border-b border-gray-200">
-            <h2 className="text-xl font-semibold mb-4 flex-shrink-0">
+          <div className="px-4 md:px-6 py-4 flex-shrink-0 border-b border-gray-200">
+            <h2 className="text-2xl font-semibold mb-4 flex-shrink-0">
               Proximity Search
             </h2>
 
@@ -130,8 +142,8 @@ export default function PolicePage() {
           </div>
 
           {/* Party List Section */}
-          <div className="px-6 py-4 flex-1 flex flex-col overflow-hidden">
-            <h2 className="text-xl font-semibold mb-4 flex-shrink-0">
+          <div className="px-4 md:px-6 py-4 flex-1 flex flex-col overflow-hidden">
+            <h2 className="text-2xl font-semibold mb-4 flex-shrink-0">
               Party List
             </h2>
 
@@ -151,7 +163,7 @@ export default function PolicePage() {
 
             {/* Party List - Scrollable */}
             {!isLoading && !isLoadingNearby && (
-              <div className="flex-1 min-h-0" id="party-list">
+              <div className="max-h-48 overflow-scroll md:flex-1 min-h-0" id="party-list">
                 <PartyList
                   parties={filteredParties}
                   onSelect={(party) => handleActiveParty(party)}
@@ -163,11 +175,11 @@ export default function PolicePage() {
         </div>
 
         {/* Right Panel - Map */}
-        <div className="flex-1 px-6 py-4 flex flex-col overflow-hidden">
-          <h2 className="text-xl font-semibold mb-4 flex-shrink-0">
+        <div className="h-[60vh] md:flex-1 px-6 py-4 flex flex-col overflow-hidden">
+          <h2 className="text-2xl font-semibold mb-4 flex-shrink-0">
             {searchAddress ? "Showing Nearby Parties" : "Showing Parties"}
           </h2>
-          <div className="flex-1 overflow-hidden">
+          <div className="h-full md:flex-1 overflow-hidden">
             <EmbeddedMap
               parties={filteredParties}
               activeParty={activeParty}
