42dfb8809025f78dd20c8f2a719ba23c4fc37e23
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-03-03 17:04:30 -0500

    fix: mock data & some font stuff

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${avenirNext.variable} font-[family-name:var(--font-avenir-next)] antialiased bg-background`}
+        className={`${avenirNext.variable} font-[family-name:var(--font-avenir-next)] antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
