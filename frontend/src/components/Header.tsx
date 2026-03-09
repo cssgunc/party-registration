ea683489db760c5777b2c1a14794e01b98fa86e5
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-03-09 16:08:10 -0400

    feat: Header in root layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import OCSLLogo from "@/components/OCSLLogo";
+"use client";
+
 import logout from "@/components/icons/log-out.svg";
 import pfp from "@/components/icons/pfp_temp.svg";
 import user from "@/components/icons/user.svg";
@@ -8,27 +9,39 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useRole } from "@/contexts/RoleContext";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import PartySmartLogo from "./PartySmartLogo";
 
-export default function Header() {
+export default function Header({ className }: { className?: string }) {
+  const { role } = useRole();
+
   return (
-    <div className="bg-primary p-4 w-full flex justify-between items-center">
-      <OCSLLogo />
+    <div
+      className={cn(
+        "bg-primary py-4 px-6 w-full flex justify-between items-center",
+        className
+      )}
+    >
+      <PartySmartLogo />
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <button className="cursor-pointer">
-            <Image src={pfp} alt="pfp" width={60} height={60} />
+            <Image src={pfp} alt="pfp" width={50} height={50} />
           </button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-60" align="end">
-          <Link href="/student/profile">
-            <DropdownMenuItem>
-              <Image src={user} alt="user" />
-              <span>Edit Profile Information</span>
-            </DropdownMenuItem>
-          </Link>
+          {role === "student" && (
+            <Link href="/student/profile">
+              <DropdownMenuItem>
+                <Image src={user} alt="user" />
+                <span>Edit Profile Information</span>
+              </DropdownMenuItem>
+            </Link>
+          )}
           <DropdownMenuItem>
             <Image src={logout} alt="logout" />
             <span>Logout</span>
