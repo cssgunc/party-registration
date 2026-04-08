ad36cbfe1dbb24ec84157d7e706e35615069ee0f
Nicolas Asanov <nicolas.a.asanov@gmail.com>
2026-04-08 17:10:00 -0400

    fix(out-of-scope): fix student page scroll. needed to test changes

--- frontend/src/app/student/new-party/page.tsx
+++ frontend/src/app/student/new-party/page.tsx
@@ -97,7 +97,7 @@ export default function RegistrationForm() {
   };
 
   return (
-    <div>
+    <div className="h-full overflow-y-auto">
       <main className="mx-4 mt-4">
         <nav className="flex items-center content pb-2 lg:hidden">
           <ArrowLeft className="h-4" />
