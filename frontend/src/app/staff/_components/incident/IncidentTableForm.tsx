a4cf13845d60cff758a42732d6b577eaf472ff3c
Mereck Fandohan <fandohan@ad.unc.edu>
2026-04-18 20:23:25 -0400

    cleaned up form styling across all forms

--- frontend/src/app/staff/_components/incident/IncidentTableForm.tsx
+++ frontend/src/app/staff/_components/incident/IncidentTableForm.tsx
@@ -285,7 +285,7 @@ export default function IncidentTableForm({
               value={formData.description ?? ""}
               onChange={(e) => updateField("description", e.target.value)}
               placeholder="Optional"
-              className=" w-full min-h-24 px-3 py-2 rounded-md border border-input bg-card text-foreground focus:outline-none focus:ring-2 focus:ring-ring resize-vertical"
+              className=" w-full min-h-24 px-3 py-2 rounded-md border border-input bg-card text-foreground focus:outline-none focus:ring-2 focus:ring-ring resize-vertical shadow-xs input-shadow transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 "
             />
             {errors.description && (
               <FieldError>{errors.description}</FieldError>
